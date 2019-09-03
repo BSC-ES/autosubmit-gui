422d849878fe6bfd2fac33ae9c837002b71bb65c
Wilmer Uruchi Ticona <wilmer.uruchi@bsc.es>
2019-09-03 11:57:15 +0200

    Clarified message in Graph to indicate when Show Monitor button shows up

--- src/components/experiment/GraphNativeRep.js
+++ src/components/experiment/GraphNativeRep.js
@@ -43,7 +43,7 @@ class GraphNativeRep extends Component {
             return (
                 <div className="card-body text-left">
                     <p className='lead'>Press <span className='badge badge-info'>Show Graph</span> to see the graph representation of the experiment.</p>
-                    <p className='lead'>If the experiment is running, press <span className='badge badge-dark'>Start Job Monitor</span> to start a live tracker of the changes on the experiment's jobs.
+                    <p className='lead'>If the experiment is <span className='badge badge-success'>RUNNING</span> and the Graph has been rendered, press <span className='badge badge-dark'>Start Job Monitor</span> to start a live tracker of the changes on the experiment's jobs.
                         This process will automatically update the graph's nodes colors and show a log of the detected changes.
                     </p>
                 </div> 
