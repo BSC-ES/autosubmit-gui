dbf9f0932dcc34c389d77fe5f7d95524c2eba49f
wuruchi <wilmer.uruchi@bsc.es>
2021-12-01 16:57:13 +0100

    Update login message

--- src/components/pages/Login.js
+++ src/components/pages/Login.js
@@ -1,10 +1,9 @@
-import React, { useContext, useEffect } from 'react';
+import React, { useContext, useEffect } from "react";
 import ExperimentContext from "../context/experiment/experimentContext";
-import queryString from 'query-string';
-import { rootAppName } from '../context/vars';
+import queryString from "query-string";
+import { rootAppName } from "../context/vars";
 
 const Login = (props) => {
-
   const experimentContext = useContext(ExperimentContext);
   const { getVerifyTicket, loggedUser } = experimentContext;
 
@@ -22,30 +21,29 @@ const Login = (props) => {
     if (loggedUser) {
       setTimeout(() => {
         props.history.push(`/${rootAppName}/?user=${loggedUser}`);
-      }, 3000)
+      }, 3000);
     }
-  }
+  };
 
   const onVerify = (ticket) => {
     //e.preventDefault();
     //console.log('Attempt to verify ' + ticket)
     getVerifyTicket(ticket);
     //e.preventDefault();
-  }
+  };
   const onCASLogin = () => {
     const _target = `https://cas.bsc.es/cas/login?service=https://earth.bsc.es/${rootAppName}/login`;
     //console.log('On CAS Log')
     window.location.href = _target;
-  }
+  };
   // console.log(loggedUser);
   // console.log(values);
   if (loggedUser) {
     actionAfter();
-    if (loggedUser === 'Failed') {
+    if (loggedUser === "Failed") {
       return (
         <div>
           Oops! We couldn't authenticate you.
-
           <p>You will be redirected after some seconds.</p>
         </div>
       );
@@ -53,17 +51,14 @@ const Login = (props) => {
       return (
         <div>
           Welcome {loggedUser}
-
-          <p>We are currently testing CAS login for Autosubmit GUI. Thanks for logging in.</p>
-
+          <p>You have authenticated through CAS.</p>
           <p>You will be redirected after some seconds.</p>
         </div>
       );
     }
-  };
-
+  }
 
   return null;
-}
+};
 
-export default Login
+export default Login;
