3e05e93d0afc01f9b8dc19e5e614d6fc92c3c148
Julian Rodrigo Berlin <julian.berlin@bsc.es>
2022-09-01 15:53:38 +0200

    update the script to launch/update Autosubmit API

--- deployment/scripts/update_launch_autosubmit_API.sh
+++ deployment/scripts/update_launch_autosubmit_API.sh
@@ -1,21 +1,32 @@
 #!/bin/bash
+. ~/.bashrc
 
-# first parameter to specify the path to the  virtual environment
-VIRTUAL_ENV="$1"
-source ${VIRTUAL_ENV}
+# PVE_PATH and PLOG are defined in the .bashrc for the virtual environment to be loaded for the API
+PVE=${PVE_PATH}
+LOG_PATH=${PLOG}
+UPDATE=false
 
-if [ $# -gt 1 ]
-then
-  if [ "$2" == "--update" ]
-  then
-    pip install autosubmit_api --upgrade
-  else
-    echo "Wrong option. Use --update to update the system before launch."
-    exit 0
-  fi
-fi
+while getopts ":e:u" opt; do
+  case "$opt" in
+    e)  PVE=$OPTARG;
+      ;;
+    u)  UPDATE=true;
+      ;;
+   esac
+done
+
+source ${PVE}
+
+# Stop current instance of unicorn
 ps aux | grep gunicorn | awk '{print $2}' | xargs -r kill
 ps aux | grep gunicorn
+
+# if update to a new version we install it from pip
+if [ "${UPDATE}" = true ]; then
+   pip install autosubmit_api --upgrade
+fi
+
+# prepare to launch
 echo "Set SECRET KEY"
 export SECRET_KEY='c&X=<dFoz^ym}_^{jvma'
 echo "Set CAS LOGIN URL"
@@ -25,5 +36,5 @@ export CAS_VERIFY_URL='https://cas.bsc.es/cas/serviceValidate'
 echo "Gunicorn starting in 3 seconds..."
 sleep 3
 LOG_NAME="$(date '+%Y%m%d%H%M')logunicorn.txt"
-echo "${LOG_NAME}"
-gunicorn --error-logfile ~/${LOG_NAME} --timeout 600 -b 0.0.0.0:8081 -w 6 autosubmit_api.app:app --daemon
+echo "${LOG_PATH}/${LOG_NAME}"
+gunicorn --error-logfile ${LOG_PATH}/${LOG_NAME} --timeout 600 -b 0.0.0.0:8081 -w 6 autosubmit_api.app:app --preload --daemon
