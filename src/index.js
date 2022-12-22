aaf824ccbcff39f761bf9fe0ec351dfbd46803dd
cgutierr <cristian.gutierrez@bsc.es>
2022-12-22 09:54:32 +0100

    Issue #58: Prevent firefox popups

--- src/index.js
+++ src/index.js
@@ -2,5 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
+// Prevent from firefox-safari pop-ups
+window.addEventListener('beforeunload', function (event) {
+  event.stopImmediatePropagation();
+});
 
 ReactDOM.render(<App />, document.getElementById('root'));
