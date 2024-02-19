f32b836e7d1db8696610c682622c5c3094f5121e
Luiggi Tenorio <luiggi.tenorio@bsc.es>
2024-02-19 14:03:38 +0100

    add cypress coverage

--- cypress.config.js
+++ cypress.config.js
@@ -3,7 +3,9 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      require('@cypress/code-coverage/task')(on, config)
+
+      return config
     }
   },
 });
