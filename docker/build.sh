0484fd4b749abe63f7982fcd6a00f18af7264616
Luiggi Tenorio <luiggi.tenorio@bsc.es>
2024-06-25 16:05:35 +0200

    update README

--- docker/build.sh
+++ docker/build.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 
-docker build \
-    --build-arg "AUTOSUBMIT_API_SOURCE=http://127.0.0.1:8000" \
-    -t autosubmit-gui --progress=plain . &> build.log
+docker build -t autosubmit-gui --progress=plain . &> build.log
