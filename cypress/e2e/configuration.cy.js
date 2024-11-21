af3b147f4dbd1a47cbda39cc2d02039dada7a58c
Luiggi Tenorio <luiggi.tenorio@bsc.es>
2024-11-21 14:54:54 +0100

    add more e2e tests

---
+++ cypress/e2e/configuration.cy.js
@@ -0,0 +1,53 @@
+describe("configuration view", () => {
+  const expid = "a000";
+
+  before(() => {
+    cy.intercept(
+      "GET",
+      Cypress.env("EXTERNAL_API") + `/v4/experiments/${expid}/runs`,
+      {
+        fixture: "api/v4/experiments/runs/minimal_run_list.json",
+      }
+    ).as("dummy_response");
+
+    cy.intercept(
+      "GET",
+      Cypress.env("EXTERNAL_API") + `/v4/experiments/${expid}/filesystem-config`,
+      {
+        body: {
+          config: {
+            key1: "value1",
+            key2: "value2",
+          }
+        }
+      }
+    ).as("dummy_response");
+
+    cy.intercept(
+      "GET",
+      Cypress.env("EXTERNAL_API") + `/v4/experiments/${expid}/runs/3/config`,
+      {
+        body: {
+          run_id: 3,
+          config: {
+            key1: "value1a1",
+            key2: "value2",
+          }
+        }
+      }
+    ).as("dummy_response");
+
+    cy.visit(`/experiment/${expid}/config`);
+    cy.wait(500);
+  }
+  )
+
+  it("configuration view", () => {
+    cy.contains("Run 3")
+    cy.contains("Current Filesystem")
+    // Div that contains key1 also have a child i tag with class fa-solid fa-circle-exclamation
+    cy.contains("key1").find(".fa-solid.fa-circle-exclamation").should("exist")
+  })
+
+}
+);
---
+++ cypress/e2e/table_view.cy.js
@@ -0,0 +1,32 @@
+describe("tree navigation", () => {
+  const expid = "a6zi";
+  before(() => {
+    cy.intercept("GET", Cypress.env("EXTERNAL_API") + "/v3/tree/*", {
+      fixture: "api/v3/tree/tree_wrappers.json",
+    }).as("dummy_response");
+
+    cy.visit(`/experiment/${expid}/table`);
+    cy.wait(500);
+  });
+
+  it("evaluate content", () => {
+
+    // There is one td tag with text equal to "SIM"
+    cy.get("td").contains("SIM").should("exist");
+
+    // There is one td tag that ends with "_SIM"
+    cy.get("td").contains(/_SIM$/).should("exist");
+
+    // Get the count of the SIM row in its sibling column
+    cy.get("td").contains("SIM").parent().children().eq(1).invoke('text').then((text) => {
+      cy.log(text);
+      let count = parseInt(text, 10);
+
+      // Get all td tags that contains "_SIM". The total should be equal to count
+      cy.get('td').filter((index, element) => {
+        return /_SIM$/.test(element.innerText);
+      }).should('have.length', count);
+    });
+
+  });
+});
